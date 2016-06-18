e23b49f29e07a0e3c41648586919117e5a75ae30
starcraft168 <ericeng168@gmail.com>
2016-06-17 18:15:14 -0700

    update to staging

--- Algorithms/binarySearchArray.js
+++ Algorithms/binarySearchArray.js
@@ -1,11 +1,11 @@
-/*
+/* STAGING HERE
  * Given a sorted array, find the index of an element
  * using a binary search algorithm.
  *
  * Example usage:
  *
  * var index = binarySearch([1, 2, 3, 4, 5], 4);
- * console.log(index); // [3]
+ * console.log(index); l// [3]
  */
 
 var binarySearch = function (array, target) {
